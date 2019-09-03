bab70f24e9200dfb863e7e753f85c1d2278d84b0
huaxueyihao <837403116@qq.com>
2019-09-03 21:07:26 +0800

    classloader

---
+++ jdk/practise_code/jvm_lecture/src/main/java/com/gardenia/jvm/classloader/MyTest8.java
@@ -0,0 +1,21 @@
+package com.gardenia.jvm.classloader;
+
+
+class FinalTest{
+
+    public static final int x = 3;
+
+    static {
+        System.out.println("FinalTest static block");
+    }
+
+
+}
+
+public class MyTest8 {
+
+    public static void main(String[] args) {
+        System.out.println(FinalTest.x);
+    }
+
+}
